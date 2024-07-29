17103982be5b7a65b4aba4050307f5f5c7bea2b7
ajay-wal871 <ajay.sharma@westagilelabs.com>
2024-07-29 12:16:49 +0530

    exception for negative number

--- main.rb
+++ main.rb
@@ -3,6 +3,10 @@ require 'byebug'
 def add_numbers(string)
 
 	numbers = string.scan(/-?\d+/)
+	sum = 0
+	numbers.each do |number|
+		throw "number #{number.to_s} is negative" if number.to_i < 0
+	end
 	byebug
 end
 
